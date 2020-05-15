a63b893663da840e9ee583389b0f360f72ee3156
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-05-15 21:39:27 +1000

    Added packages

--- Chapter_03.R
+++ Chapter_03.R
@@ -1,30 +1,12 @@
 # lassign library path
 .libPaths("C:/Users/90946112/R/win-library/3.6.2")
 
-# Load packages for chapter 2
+# Load packages for chapter 3
 library(sf)          # classes and functions for vector data
 library(raster)      # classes and functions for raster data
-library(spData)        # load geographic data
-library(spDataLarge)   # load larger geographic data
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+library(dplyr)
+library(stringr)
+library(spData)
 
 
 
