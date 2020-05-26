d89af0e8e0d649c351fe5e6574a0ce8704031313
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-05-26 13:24:05 +1000

    Visualised fires from 2018-19 and 2019-20

--- FireHistory_Application.R
+++ FireHistory_Application.R
@@ -4,6 +4,7 @@
 # Load packages for chapter 2
 library(sf)          # classes and functions for vector data
 library(sp)
+library(spData)
 
 firehist<-st_read("Fire_NPWS_FireHistory.shp")
 firehist
@@ -16,8 +17,14 @@ firehist$burntype<-as.factor(substr(firehist$Label,9,length(firehist$Label)))
 
 # Create df subset from year, including only fires from 2018-2019
 recent<-firehist[firehist$year>2017,]
-plot(recent)
+# plot(recent)
+fire_2018 <- recent[recent$FireName == "MER Bournda Sth LMZ -Tura Beach",]
 
 
-
-
+# isolate just Australia and map recent fires over NSW
+world_aus <- world[world$name_long == "Australia", ]
+aus <- st_union(world_aus)
+# expandBB = c(bottom, left, top, right)
+plot(aus, expandBB = c( -0.18, -0.85, -0.51, -0.1))
+plot(recent["PerimeterM"], add = TRUE, col = recent$year, border=NA)
+legend( "topright", legend = c("2018","2019"), col= c("red", "green"), lwd=10)
