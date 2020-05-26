a8f320d2c3d16fa6e633e553e7571e28f44b1ebf
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-05-26 12:35:40 +1000

    Resolve year issue by converting to numeric instead of date

--- FireHistory_Application.R
+++ FireHistory_Application.R
@@ -9,14 +9,14 @@ firehist<-st_read("Fire_NPWS_FireHistory.shp")
 firehist
 
 # Grab just the first year listed. It looks like the original dataset describes year as the couplet describing fire season, i.e. "1996-97". Need to double check***
-firehist$year<-as.Date(substr(firehist$Label,1,4),as.year)
+firehist$year<-as.numeric(substr(firehist$Label,1,4),format="%Y")
 
 # Grab burn type from Label and convert to factor
 firehist$burntype<-as.factor(substr(firehist$Label,9,length(firehist$Label)))
 
 # Create df subset from year, including only fires from 2018-2019
 recent<-firehist[firehist$year>2017,]
-
+plot(recent)
 
 
 
