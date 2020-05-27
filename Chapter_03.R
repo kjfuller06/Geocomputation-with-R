07a937532ba3f21bcbaefd395bbaf85f52bb2308
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-05-27 13:37:05 +1000

    Continue on to 3.2

--- Chapter_03.R
+++ Chapter_03.R
@@ -8,8 +8,15 @@ library(dplyr)
 library(stringr)
 library(spData)
 
+# what methods are available for sf data?
+methods(class = "sf")
 
-
+# check out the df in the sf
+dim(world) # it is a 2 dimensional object, with rows and columns
+nrow(world) # how many rows?
+ncol(world)
+world_df = st_drop_geometry(world)
+class(world_df)
 
 
 
