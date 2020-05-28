da09533be5f016c13260057acce620b4bad1af80
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-05-28 12:40:21 +1000

    Brainstorming for next task

--- FireHistory_Application.R
+++ FireHistory_Application.R
@@ -28,3 +28,23 @@ aus <- st_union(world_aus)
 plot(aus, expandBB = c( -0.18, -0.85, -0.51, -0.1))
 plot(recent["PerimeterM"], add = TRUE, col = recent$year, border=NA)
 legend( "topright", legend = c("2018-19","2019-20"), col= c("red", "green"), lwd=10)
+
+# tasks
+# 1) extract fire history for 1970-present
+# 2) Combine fire polygons by year
+# 3) Convert the year layers into rasters
+#     b) What about years in which a pixel burned twice?
+# 4) Calculate the values for fire frequency- use number of fires since 1970?
+#     a) can also look into "sensored fire intervals", suggested by Matthias
+
+## alternatively:
+# 3) Create a stack of polygons and generate the raster file directly from this. Not sure if this is possible but it'd be nice and probably less computationally expensive.
+
+
+
+
+
+
+
+
+
